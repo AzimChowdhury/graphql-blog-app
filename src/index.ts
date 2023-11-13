e3950e62207389fdb506bed8a4568de296bc842a
Azim Chowdhury <azimchy994@gmail.com>
2023-11-13 21:36:14 +0600

    prisma and postgresql

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,44 @@
-const cName: string = "next level";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
-console.log(cName);
+const typeDefs = `#graphql
+
+  type Book {
+    title: String
+    author: String
+  }
+ 
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+(async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
+})();
